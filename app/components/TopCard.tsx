6937ca91cead74b6917bece51ad98a57ced22a9d
fellowfy-fy <zhoraawesome@gmail.com>
2024-09-03 18:51:58 +0300

    Some changes

---
+++ app/components/TopCard.tsx
@@ -0,0 +1,44 @@
+"use client";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import LinearWithValueLabel from "./ProgressBar";
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+  >
+    •
+  </Box>
+);
+
+export default function TopCard() {
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography gutterBottom sx={{ color: "text.secondary", fontSize: 14 }}>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="div">
+          be{bull}nev{bull}o{bull}lent
+        </Typography>
+        <Typography sx={{ color: "text.secondary", mb: 1.5 }}>
+          adjective
+        </Typography>
+        <Typography variant="body2">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        {" "}
+        <LinearWithValueLabel />
+      </CardActions>
+    </Card>
+  );
+}
